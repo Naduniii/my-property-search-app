ac4eb149b2a7cf8ad0357d48289935f6c3125fc3
Naduniii <naduniameesha17141@gmail.com>
2026-01-02 17:05:43 +0530

    Add property list component

---
+++ src/components/PropertyList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropertyCard from './PropertyCard';
+
+export default function PropertyList ({properties, favorites, addFavorite }){
+    if (properties.length === 0){
+        return <p>No properties match your criteria.</p>
+    }
+    return (
+        <div className= "property-list" style={{display: 'grid', gap:'1rem' , gridTemplateColumns: 'repeat (auto-fit, minmax (280px, 1fr))'}}>
+        {properties.map ((property)=> (
+            <PropertyCard
+            key = {property.id}
+            property = {property}
+            isFavorited = {favorites.some((fav) => fav.id === property.id)}
+            addFavorite={addFavorite}
+            removeFavourite = {removeFavourite}
+            />
+
+        ))}
+        </div>
+    );
+}
